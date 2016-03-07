7597f3259d627df2cb87569ad4a95b54c065fdbf
leizhengyu <6zhyong@163.com>
2016-03-07 19:31:25 +0800

    update

--- test.py
+++ test.py
@@ -4,30 +4,36 @@ import string
 import sys
 import os
 import urllib
-import urllib3
+import urllib3,certifi
 from bs4 import BeautifulSoup
 import lxml.etree
 import lxml.html as x
 
 
 
-url = 'http://www.baidu.com'
+url = 'http://detail.1688.com/offer/42769715160.html'
+headers = {'User-Agent':'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET4.0C; .NET4.0E)'}
 
-pool = urllib3.PoolManager()
+pool = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
 
-r = pool.request("GET",url)
+r = pool.request("GET",url,headers=headers)
 
-html = r.data.decode()
-
+html = r.data
 ##soup = BeautifulSoup(html,"lxml")
 ##
 ##print(soup.prettify())
-f=open('test.html','rb')
-c=f.read()
-doc=x.document_fromstring(c)
-body=doc.xpath('//*[@id="sm-maindata"]/div')[0]
-alist=body.xpath('//div[@class="imgofferresult-mainBlock"]/div/a')
+##f=open('test.html','rb')
+##c=f.read()
+doc=x.document_fromstring(html)
+alist=doc.xpath('//li[@data-page-name="contactinfo"]/a')
+url=alist[0].attrib['href']
 
-for a in alist:
-    tree=lxml.etree.ElementTree(a)
-    print(tree.getpath(a))
+r = pool.request("GET",url,headers=headers)
+doc=x.document_fromstring(r.data)
+contactname=doc.xpath('//div[@class="contact-info"]/dl/dd/a')[0].text
+print(contactname)
+contactdesc=doc.xpath('//div[@class="contcat-desc"]/dl/dd')
+for desc in contactdesc:
+    print(desc.text)
+
+
