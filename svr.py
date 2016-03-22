123b74202154973755ba43a64ef691632a672b4e
leizhengyu <6zhyong@163.com>
2016-03-22 10:46:45 +0800

    init data

---
+++ svr.py
@@ -0,0 +1,24 @@
+#创建SocketServerTCP服务器：
+import socketserver
+from socketserver import StreamRequestHandler as SRH
+from time import ctime
+
+host = '127.0.0.1'
+port = 9999
+addr = (host,port)
+
+
+class Servers(SRH):
+    def handle(self):
+        print('got connection from ',self.client_address)
+
+        while True:
+            data = self.request.recv(1024)
+            if not data:
+                break
+            print(data)
+            print("RECV from ", self.client_address[0])
+            self.request.send(data)
+print('server is running....')
+server = socketserver.ThreadingTCPServer(addr,Servers)
+server.serve_forever()
